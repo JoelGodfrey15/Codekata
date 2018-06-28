1cf57f2c03fb64610164fb55876b34a5c72414a5
JoelGodfrey15 <40669479+JoelGodfrey15@users.noreply.github.com>
2018-06-28 23:30:38 +0530

    Add files via upload

---
+++ Nooftimes.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Joel Godfrey
+#
+# Created:     28-06-2018
+# Copyright:   (c) Joel Godfrey 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    a=int(input('Enter number of times:'))
+    for i in range(a):
+        print('Hello')
+
+if __name__ == '__main__':
+    main()
